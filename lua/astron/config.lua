d218fae4286f5687456be9d80af9227851be53cc
bryant-the-coder <bryantfoo99@gmail.com>
2022-02-12 21:54:36 +0800

    Update config.lua

--- lua/astron/config.lua
+++ lua/astron/config.lua
@@ -3,18 +3,20 @@ local config
 vim = vim or { g = {}, o = {} }
 
 local function opt(key, default)
+	key = "substrata_" .. key
 	if vim.g[key] == nil then
-		return default
+	  return default
 	end
 	if vim.g[key] == 0 then
-		return false
+	  return false
 	end
+
 	return vim.g[key]
-end
+  end
 
 config = {
 	italic = opt("enable_italic", true),
-	italic_comments = opt("italic_keywords", true) and "italic" or "NONE",
+	italic_comments = opt("italic_comments", true) and "italic" or "NONE",
 	italic_keywords = opt("italic_keywords", true) and "italic" or "NONE",
 	italic_functions = opt("italic_function", false) and "italic" or "NONE",
 	italic_variables = opt("italic_variables", true) and "italic" or "NONE",

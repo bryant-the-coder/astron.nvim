e8902f82df5db38f6e6a68f1e8e5519ba644fcc7
bryant-the-coder <bryantfoo99@gmail.com>
2022-02-21 08:19:20 +0800

    changed smth

--- lua/astron/treesitter.lua
+++ lua/astron/treesitter.lua
@@ -47,7 +47,7 @@ local treesitter = {
 	TSLiteral = { fg = C.fg },
 	TSURI = { fg = C.fg },
 	TSTag = { fg = C.red },
-	TSComment = { fg = C.greu_2 },
+	TSComment = { fg = C.gray_2 },
 	TSTagDelimiter = { fg = C.fg },
 	rainbowcol1 = { fg = "Gold" },
 	rainbowcol2 = { fg = "Orchid" },
